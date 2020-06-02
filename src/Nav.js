ddb07e9c577e412ddd84403e3d163222b1d26fb2
Vito60 <randallw1996@gmail.com>
2020-06-02 17:38:43 -0400

    updated styling

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -45,18 +42,8 @@ export default function Nav() {
                     Randall Williams
                 </Typography>
         <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick} >
-          Menu
+            Welcome
         </Button>
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleClose}><Link to='/'>Home</Link></MenuItem>
-          <MenuItem onClick={handleClose}><Link to='/'>Resume</Link></MenuItem>
-        </Menu>
             </Toolbar>
       </AppBar>
       </div>
