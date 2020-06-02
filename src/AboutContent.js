8337223a989cb0b3a9ef9c993b15e0b942b3a22e
Vito60 <randallw1996@gmail.com>
2020-06-01 21:16:33 -0400

    added basic file structure and styling

---
+++ src/AboutContent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography';
+
+export default function AboutContent() {
+    return (
+        <div>
+            <Typography>
+                Experienced in React, Redux, JavaScript, and Rails based 
+                programming with a background in team based environments. 
+                Certified in crisis negotiating this training has provided 
+                me with strong skills in problem solving and a resilient 
+                attitude during stressful situations.  
+            </Typography>
+        </div>
+    )
+}
---
+++ src/Home.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import AboutNav from './AboutNav'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import PortfolioImage from './PortfolioImage.png'
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: "100%",
+    height: "100%"
+  },
+  media: {
+    height: 800,
+  },
+  content: {
+    textAlign: 'center',
+    justifyContent: 'center'
+  }
+});
+
+export default function Home() {
+    const classes = useStyles();
+  
+    return (
+        <Container maxWidth="sm">
+      <Card className={classes.root}>
+        <CardActionArea className={classes.root}>
+          <CardMedia
+            className={classes.media}
+            image={PortfolioImage}
+            title="Contemplative Reptile"
+          />
+          <CardContent className={classes.content}>
+            <Typography gutterBottom variant="h5" component="h2">
+              Full Stack Developer
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions className={classes.content}>
+         <AboutNav/>
+        </CardActions>
+      </Card>
+      </Container>
+    );
+  }
